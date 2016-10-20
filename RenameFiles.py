c15501c2d3890409da0ff0c905b5f228f3525958
clafuno <clarafurionov@gmail.com>
2016-10-20 23:56:13 +0200

    Fixed folder path in listdir

--- RenameFiles.py
+++ RenameFiles.py
@@ -1,8 +1,16 @@
 # Udacity course - PROGRAMMING FOUNDATIONS WITH PYTHON
 # Programm 1.1: Rename files
+import os
+
 
 def rename_files():
 	# 1st get files names from a folder
-	file_list = os.listdir("/Users/clara/Descargas/prank")
+	file_list = os.listdir(os.path.expanduser('/Users/clara/Downloads/prank'))
 	print(file_list)
 	# 2nd for each file, rename file name
+	for file_name in file_list:
+		#os.rename(old_name, new_name)
+		os.rename(file_name,)
+
+
+rename_files()
