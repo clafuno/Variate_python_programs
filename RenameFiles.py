25cbd35af9edfcd005041f34fc6daaf65fd61f07
clafuno <clarafurionov@gmail.com>
2016-10-21 00:10:03 +0200

    Added comments w/ useful functions to RenameFiles.py

--- RenameFiles.py
+++ RenameFiles.py
@@ -8,15 +8,18 @@ def rename_files():
 	# 1st get files names from a folder
 	file_list = os.listdir(os.path.expanduser('/Users/clara/Downloads/prank'))
 	#print(file_list)
-	#saved_path = os.getcwd()
 	os.chdir(os.path.expanduser('/Users/clara/Downloads/prank'))
+	
 	# 2nd for each file, rename file name
 	for file_name in file_list:
-		#os.rename(old_name, new_name)
-		#string.translate(s, table[, deletechars])
 		os.rename(file_name, file_name.translate(None,"0123456789"))
 
+rename_files() # Execute the function
 
-rename_files()
 
-
+# EXTRA NOTES
+# 
+# os.getcw()     -> gets the path of the current directory
+# os.chdir(path) -> changes the working directory to the specified path
+# os.rename(old_name, new_name)
+# string.translate(s, table[, deletechars]) -> delete certain chars from a string
