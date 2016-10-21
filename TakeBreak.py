22cbc81cff9e0aa43c59a2a05b4b624c6c00731a
clafuno <clarafurionov@gmail.com>
2016-10-21 20:04:23 +0200

    Configured TakeBreak to open the browser after X time

--- TakeBreak.py
+++ TakeBreak.py
@@ -1,5 +1,17 @@
 # Udacity course - PROGRAMMING FOUNDATIONS WITH PYTHON
 # Programm 1: Take a break
-import webbrowser
 
-webbrowser.open("https://www.google.com")
+import webbrowser
+import time
+
+total_breaks = 3
+break_count = 0
+
+print("This program started on "+time.ctime())
+#time.ctime provides date and hour from the computer.
+
+while (break_count < total_breaks):
+	time.sleep(10) #Wait for x seconds
+	webbrowser.open("https://www.google.com")
+	break_count = break_count + 1
+
