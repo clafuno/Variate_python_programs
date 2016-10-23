d89f99470c34ca18751c819f8ae6a67c985adb14
clafuno <clarafurionov@gmail.com>
2016-10-24 00:15:39 +0200

    Started branch function

--- DrawTree.py
+++ DrawTree.py
@@ -1,6 +1,15 @@
 # Drawing a tree using the turtle tool
 import turtle
 
+def brach(turtle,angle,length,scale):
+	t.forward(length)
+	t.right(angle)
+	# Insert blanch here (https://discussions.udacity.com/uploads/default/12490/c605ef44e8ec28fc.png)
+	t.forward(length/2)
+	t.left(180)
+	t.forward(length/2)
+	t.left(angle+180+angle)
+	t.forward(length)
 
 def draw():
 	window = turtle.Screen()
@@ -11,7 +20,7 @@ def draw():
 
 	t.left(90)
 	t.color("green")
-	
+
 
 
 
