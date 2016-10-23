84f04c78f9cee8e7a246bda36791f709ffa79ea0
clafuno <clarafurionov@gmail.com>
2016-10-23 23:57:50 +0200

    Added DrawTree file

---
+++ DrawTree.py
@@ -0,0 +1,18 @@
+# Drawing a tree using the turtle tool
+import turtle
+
+
+def draw():
+	window = turtle.Screen()
+	window.bgcolor("white")
+
+	t = turtle.init()
+	t.speed(10)
+
+	t.left(90)
+	t.color("green")
+	
+
+
+
+	window.exitonclick()
