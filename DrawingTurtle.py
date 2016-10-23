6b6d49de1703f1df90f6785a88e49f6fcf0b0afa
clafuno <clarafurionov@gmail.com>
2016-10-23 11:43:00 +0200

    Added function draw_square

--- DrawingTurtle.py
+++ DrawingTurtle.py
@@ -1,2 +1,25 @@
 # Udacity course - PROGRAMMING FOUNDATIONS WITH PYTHON
 # Programm 3: Drawing Turtle in Python
+
+import turtle
+
+def draw_square():
+#Create the window
+	window = turtle.Screen()
+	window.bgcolor("white")
+
+#Grab the turtle and indicate the movement to draw a square
+	tur = turtle.Turtle()
+	tur.forward(100)
+	tur.right(90)
+	tur.forward(100)
+	tur.right(90)
+	tur.forward(100)
+	tur.right(90)
+	tur.forward(100)
+	tur.right(90)
+
+	window.exitonclick()
+
+draw_square()
+
