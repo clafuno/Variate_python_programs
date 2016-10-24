77901acbc31d1a26ec0243222f8a62b44d078681
clafuno <clarafurionov@gmail.com>
2016-10-24 16:30:28 +0200

    Function branch draws one level

--- DrawTree.py
+++ DrawTree.py
@@ -1,27 +1,27 @@
 # Drawing a tree using the turtle tool
 import turtle
 
-def brach(turtle,angle,length,scale):
-	t.forward(length)
+def branch (t,angle,length):
+	init = t.pos()
 	t.right(angle)
-	# Insert blanch here (https://discussions.udacity.com/uploads/default/12490/c605ef44e8ec28fc.png)
-	t.forward(length/2)
-	t.left(180)
-	t.forward(length/2)
-	t.left(angle+180+angle)
+	t.forward(length)
+	t.goto(init)
+	t.left(2*angle)
 	t.forward(length)
 
 def draw():
 	window = turtle.Screen()
 	window.bgcolor("white")
 
-	t = turtle.init()
-	t.speed(10)
+	t = turtle.Turtle()
+	t.speed(1)
+	t.color("#088A08")
 
 	t.left(90)
-	t.color("green")
-
-
+	t.forward(100)
+	branch(t,30,40)
 
 
 	window.exitonclick()
+
+draw()
