a8af5af7662bd9beb9db70a3d664eb0fed6bc993
clafuno <clarafurionov@gmail.com>
2016-10-25 00:17:44 +0200

    Updated branch function

--- DrawTree.py
+++ DrawTree.py
@@ -1,15 +1,39 @@
 # Drawing a tree using the turtle tool
 import turtle
 
-def branch (t,angle,length):
-	init = t.pos()
+def branch (t,angle,length,end_level):
+	global level
+	if level > end_level:
+		return
+
+	pos.append(t.pos())
+	print len(pos)
 	t.right(angle)
 	t.forward(length)
-	t.goto(init)
-	t.left(2*angle)
+	level = level + 1
+	print level, end_level
+	branch(t,angle,2*length/3,end_level)
+
+	t.up() #Stop drawing
+	t.goto(pos.pop()) #Go to prev intersection
+	print len(pos)
+	level = level - 1
+	print level, end_level
+	t.left(angle)
+	t.down() #Start drawing
 	t.forward(length)
 
+	if len(pos) < end_level:
+		branch(t,angle,length,end_level)
+
+
+
 def draw():
+	global pos
+	global level
+	pos = []
+	loc = []
+	level = 0
 	window = turtle.Screen()
 	window.bgcolor("white")
 
@@ -19,8 +43,7 @@ def draw():
 
 	t.left(90)
 	t.forward(100)
-	branch(t,30,40)
-
+	branch(t,30,100,5)
 
 	window.exitonclick()
 
