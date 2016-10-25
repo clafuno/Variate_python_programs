6f9181064c59fd510820db037541ab770342d544
clafuno <clarafurionov@gmail.com>
2016-10-26 00:09:48 +0200

    Working version. Added level variable as an argument for branch function

--- DrawTree.py
+++ DrawTree.py
@@ -1,49 +1,47 @@
 # Drawing a tree using the turtle tool
 import turtle
 
-def branch (t,angle,length,end_level):
-	global level
+def branch (t,angle,length,level,end_level):
 	if level > end_level:
 		return
 
+	alpha = angle
 	pos.append(t.pos())
-	print len(pos)
+	#print len(pos)
 	t.right(angle)
 	t.forward(length)
 	level = level + 1
-	print level, end_level
-	branch(t,angle,2*length/3,end_level)
+	#print level, end_level
+
+	branch(t,angle,2*length/3,level,end_level) #Right branches
 
 	t.up() #Stop drawing
 	t.goto(pos.pop()) #Go to prev intersection
-	print len(pos)
-	level = level - 1
-	print level, end_level
+	#print len(pos)
+	#level = level - 1
+	#print level, end_level
 	t.left(angle)
 	t.down() #Start drawing
 	t.forward(length)
 
-	# if len(pos) < end_level:
-	# 	branch(t,angle,length,end_level)
-
+	branch(t,-angle,2*length/3,level,end_level) #Left branches
 
 
 def draw():
 	global pos
-	global level
 	pos = []
-	loc = []
 	level = 0
 	window = turtle.Screen()
 	window.bgcolor("white")
 
 	t = turtle.Turtle()
-	t.speed(1)
+	t.speed(10)
 	t.color("#088A08")
+	t.goto(0,-100)
 
 	t.left(90)
 	t.forward(100)
-	branch(t,30,100,5)
+	branch(t,30,100,level,5)
 
 	window.exitonclick()
 
